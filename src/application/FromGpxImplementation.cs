926a4c9941af74f2b75739d203853c40846989f9
fvanrysselberghe <filip@byte-atelier.be>
2024-11-17 10:52:07 +0100

    use actual reference point

--- src/application/FromGpxImplementation.cs
+++ src/application/FromGpxImplementation.cs
@@ -22,7 +22,23 @@ public class FromGpxImplementation : IFromGpxImplementation
         if (attempts.Count() < 1)
             throw new NoLocationException();
 
-        return new GameInputByLocation("123", _Repository.Retrieve(0, 0), new List<WaypointCandidate>(attempts));
+        var referencePoint = GetReferencePoint(attempts);
+        return new GameInputByLocation("123", _Repository.Retrieve(referencePoint.Item1, referencePoint.Item2), new List<WaypointCandidate>(attempts));
+    }
+
+    /// <summary>
+    /// Determines a reference point (i.e., location) that can be used to determine the course which is being played.
+    /// From all possible approaches (most central, first, last...), we currently implement the simplest i.e., the first
+    /// point. We thus assume that the user starts tracking his locations at the start of his game. 
+    /// 
+    /// Pre-condition: attempts is not empty
+    /// </summary>
+    /// <param name="attempts">Locations tracked</param>
+    /// <returns></returns>
+    private Tuple<double, double> GetReferencePoint(IEnumerable<WaypointCandidate> attempts)
+    {
+        var first = attempts.First();
+        return new Tuple<double, double>(first.Longitude, first.Latitude);
     }
 
     private readonly IGolfCourseRepository _Repository;
