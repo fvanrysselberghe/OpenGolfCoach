c84e6f7016fd950dffd52e8eb21e317044afd296
fvanrysselberghe <filip@byte-atelier.be>
2025-06-16 21:15:49 +0200

    simplify

--- tests/Application.UnitTests/StubGolfRepository.cs
+++ tests/Application.UnitTests/StubGolfRepository.cs
@@ -3,10 +3,8 @@ using OpenGolfCoach.Application.Interfaces;
 using OpenGolfCoach.Application.Models;
 
 namespace OpenGolfCoach.Application.UnitTests;
-public class StubGolfRepository : IGolfCourseRepository
+
+public class StubGolfRepository : IGolfCourseRetriever
 {
-    public GolfCourse Retrieve(Coordinate location)
-    {
-        return new GolfCourse("Bushwood Country Club");
-    }
+    public GolfCourse Retrieve(Coordinate location) => new GolfCourse { Name = "Bushwood Country Club" };
 }
