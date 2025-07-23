bb5eef508472b307456fe5cccc48ff0fa82f8ba8
fvanrysselberghe <filip@byte-atelier.be>
2025-07-23 22:07:49 +0200

    Correct Moq to Protected and add some details for a failing test

--- tests/Infrastructure.UnitTests/OverpassApiGolfCourseRetrieverTest.cs
+++ tests/Infrastructure.UnitTests/OverpassApiGolfCourseRetrieverTest.cs
@@ -1,7 +1,10 @@
+using System.Globalization;
 using System.Net;
 using System.Net.Http.Json;
 using System.Text.Json;
 using Moq;
+using Moq.Protected;
+using NetTopologySuite.Geometries;
 using OpenGolfCoach.Application.Models;
 using OpenGolfCoach.Infrastructure.OpenStreetmap;
 
@@ -53,10 +56,10 @@ public class OpenStreetMapGolfCourseRetrieverTest
         var holeNumber = 0;
         foreach (var hole in course.Holes)
         {
-            var overpassLine = new List<Geometry>();
+            var overpassLine = new List<OpenGolfCoach.Infrastructure.OpenStreetmap.Geometry>();
             foreach (var point in hole.TargetLine)
             {
-                overpassLine.Add(new Geometry
+                overpassLine.Add(new OpenGolfCoach.Infrastructure.OpenStreetmap.Geometry
                 {
                     Longitude = point.X,
                     Latitude = point.Y
@@ -89,14 +92,21 @@ public class OpenStreetMapGolfCourseRetrieverTest
         var expected = new GolfCourse()
         {
             Name = "Work in progress",
+            Holes = [new Hole { Number = 1, Par = 3, TargetLine = [new Coordinate(51.1651252, 4.5711302), new Coordinate(51.1653774, 4.569289)], Strokeindex = 6 },
+                new Hole{Number = 2, Par = 3, TargetLine = [new Coordinate(51.1656968, 4.5685237), new Coordinate(51.1660897, 4.5675215)], Strokeindex = 8},
+                new Hole{Number=3, Par=4, TargetLine = [new Coordinate(51.1666895, 4.566289), new Coordinate(51.1661327, 4.5685564), new Coordinate(51.1667731, 4.5702201)], Strokeindex = 1}]
         };
 
-        var response = new HttpResponseMessage(HttpStatusCode.OK);
-        response.Content = JsonContent.Create(ToOverpass(expected));
+        var response = new HttpResponseMessage(HttpStatusCode.OK)
+        {
+            Content = JsonContent.Create(ToOverpass(expected))
+        };
 
         var mock = new Mock<HttpMessageHandler>();
-        mock.As<IExposedHttpMessageHandler>()
-        .Setup(mockedHandler => mockedHandler.SendAsync(It.IsAny<HttpRequestMessage>(), It.IsAny<CancellationToken>()).Result).Returns(response);
+        mock.Protected()
+            .As<IExposedHttpMessageHandler>()
+            .Setup(mockedHandler => mockedHandler.SendAsync(It.IsAny<HttpRequestMessage>(), It.IsAny<CancellationToken>()).Result)
+            .Returns(response);
 
         HttpClient client = new(mock.Object);
 
